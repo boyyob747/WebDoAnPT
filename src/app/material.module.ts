26313549a8d63ded9d3afa57bde758436ecc9148
boyyob747 <boysavan@gmail.com>
2018-05-06 03:04:59 +0700

    28%

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,MatOptionModule, 
+import { MatRadioModule,MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,MatOptionModule, 
   MatSortModule, MatTableModule , MatToolbarModule ,MatCardModule,MatFormFieldModule,MatSelectModule, MatMenuModule ,
    MatSidenavModule ,MatDividerModule,MatListModule,MatTabsModule,MatProgressBarModule,MatDatepickerModule,MatNativeDateModule,MatIconModule} from '@angular/material';
   import { RouterModule } from '@angular/router'; // we also need angular router for Nebular to function properly
   import { NbSidebarModule, NbLayoutModule } from '@nebular/theme';
   import { ChartsModule } from 'ng2-charts/ng2-charts';
 @NgModule({
-  imports: [ChartsModule,MatDatepickerModule,MatOptionModule,MatCardModule,MatInputModule,MatIconModule,
+  imports: [MatRadioModule,ChartsModule,MatDatepickerModule,MatOptionModule,MatCardModule,MatInputModule,MatIconModule,
     MatTableModule,
     MatPaginatorModule,
     RouterModule, // RouterModule.forRoot(routes, { useHash: true }), if this is your app.module
@@ -16,7 +16,7 @@ import { MatInputModule, MatPaginatorModule, MatProgressSpinnerModule,MatOptionM
     MatSortModule,
     MatProgressSpinnerModule,
     MatToolbarModule,MatMenuModule,MatSidenavModule,MatFormFieldModule,MatSelectModule,MatDividerModule,MatListModule,MatTabsModule,MatProgressBarModule,MatNativeDateModule],
-  exports: [ChartsModule,MatDatepickerModule,MatIconModule,MatOptionModule,MatCardModule,MatInputModule,
+  exports: [MatRadioModule,ChartsModule,MatDatepickerModule,MatIconModule,MatOptionModule,MatCardModule,MatInputModule,
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
